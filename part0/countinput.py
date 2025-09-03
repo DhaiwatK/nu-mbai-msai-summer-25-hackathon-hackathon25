9e3e9da30dfdc0984c541ae8ae0ea654f38f14c5
Dhaiwat Kabaria <dhaiwat.kabaria@kellogg.northwestern.edu>
2025-09-03 16:38:06 -0500

    Hackathon

---
+++ part0/countinput.py
@@ -0,0 +1,17 @@
+"""
+Authors: Deepesh K, Dhaiwat K
+Creation date: 09/03/2025
+Countinput
+"""
+
+def countchars(st):
+    if not st:  # Edge case: empty input
+        return 0
+
+    exclude = {' ', '.', '!', ','}
+    return sum(1 for ch in st if ch not in exclude)
+
+if __name__ == "__main__":
+    user_input = input("Enter a string: ")
+    result = countchars(user_input)
+    print(result)
